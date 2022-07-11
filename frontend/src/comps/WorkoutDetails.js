1adc135a8cd6f407827c98f90fafeeb7dc517ccd
MLDODSON <paola@dominicansoul.com>
2022-07-11 10:59:15 -0500

    Branch - 9
    Branch - 9. Creating and styling the New Workout form.

--- frontend/src/comps/WorkoutDetails.js
+++ frontend/src/comps/WorkoutDetails.js
@@ -2,7 +2,7 @@ const WorkoutDetails = ({ workout }) => {
     return (
         <div className="workout-details">
             <h3>{workout.title}</h3>
-            <p><strong>Load (kg): </strong>{workout.load}</p>
+            <p><strong>Load (lb): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p><strong>Sets: </strong>{workout.sets}</p>
             <p>{workout.createdAt}</p>
