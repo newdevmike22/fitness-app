9c7fb8a09f753b9fd95caf0d7f87bed761f21fc7
MLDODSON <paola@dominicansoul.com>
2022-07-28 10:38:24 -0500

    Branch - 18
    Branch - 18. Logging users in.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,10 +7,20 @@ const createToken = (_id) => {
 
 // login user
 const loginUser =  async (req, res) => {
-    res.json({msg: "login user"})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+      } catch (error) {
+        res.status(400).json({error: error.message})
+      }
+
 };
-
-
 
 // signup user
 const signupUser =  async (req, res) => {
@@ -23,9 +33,9 @@ const signupUser =  async (req, res) => {
         const token = createToken(user._id)
 
         res.status(200).json({email, token})
-    } catch (error) {
+      } catch (error) {
         res.status(400).json({error: error.message})
-    }
+      }
 
 };
 
