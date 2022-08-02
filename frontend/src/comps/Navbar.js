0ddd3da1005fbca0e22ee2efd80fb53ea6cb375f
MLDODSON <paola@dominicansoul.com>
2022-08-02 12:33:18 -0500

    Branch - 22
    Branch - 22. Creating the useLogout hook for the users.

--- frontend/src/comps/Navbar.js
+++ frontend/src/comps/Navbar.js
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+    const { logout } = useLogout();
+
+    const handleClick = () => {
+        logout()
+    }
+
     return (
         <header>
             <div className="container">
@@ -8,6 +15,9 @@ const Navbar = () => {
                     <h1>Fit Bud</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log Out</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Sign Up</Link>
